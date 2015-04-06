23e42fac363263cd979f37d59bcba36706c0605a
Jonathan Wilsson <jonathan.wilsson@gmail.com>
2015-04-06 14:14:09 +0200

    Allow comments in .lesshintrc

--- src/lesshint.js
+++ src/lesshint.js
@@ -3,6 +3,7 @@
 var fs = require('fs');
 var gonzales = require('gonzales-pe');
 var RcFinder = require('rcfinder');
+var stripJsonComments = require('strip-json-comments');
 
 var lesshint = function (path, options) {
     var config;
@@ -10,11 +11,19 @@ var lesshint = function (path, options) {
 
     if (!options.c || !options.config) {
         rcfinder = new RcFinder('.lesshintrc', {
+            loader: function (path) {
+                var data = fs.readFileSync(path, 'utf8');
 
+                data = stripJsonComments(data);
+
+                return JSON.parse(data);
+            }
         });
 
         config = rcfinder.find(__dirname);
     }
+
+    console.log(config);
 
     fs.readFile(path, 'utf8', function (err, data) {
         var result;
