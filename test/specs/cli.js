5b42647812dbedd2b05157c0315dce25ac916041
Jonathan Wilsson <jonathan.wilsson@bighat.se>
2015-04-27 13:01:17 +0200

    Exit with proper status codes

--- test/specs/cli.js
+++ test/specs/cli.js
@@ -1,12 +1,15 @@
 var assert = require('assert');
-var path = require('path');
+var rewire = require('rewire');
 var sinon = require('sinon');
+var path = require('path');
 
 describe('cli', function () {
-    var cli = require('../../lib/cli');
+    var cli = rewire('../../lib/cli');
 
     beforeEach(function() {
         sinon.stub(process.stderr, 'write');
+
+        cli.__set__('exit', function () {});
     });
 
     afterEach(function () {
@@ -16,26 +19,85 @@ describe('cli', function () {
     });
 
     it('should print error on invalid config file', function () {
+        var result;
+
         sinon.spy(console, 'error');
 
-        cli({
+        result = cli({
             args: ['test.less'],
             config: path.resolve(process.cwd() + '/test/data/config/invalid.json')
         });
 
-        // We can't really be sure of the JSON.parse() error message, so we'll just check that the beginning is correct
-        assert(console.error.getCall(0).args[0].indexOf('Something\'s wrong with the config file. Error: ') === 0);
-        console.error.restore();
+        return result.fail(function () {
+            // We can't really be sure of the JSON.parse() error message, so we'll just check that the beginning is correct
+            assert(console.error.getCall(0).args[0].indexOf('Something\'s wrong with the config file. Error: ') === 0);
+            console.error.restore();
+        });
     });
 
     it('should print error when no files are passed', function () {
+        var result;
+
         sinon.spy(console, 'error');
 
-        cli({
+        result = cli({
             args: []
         });
 
-        assert(console.error.getCall(0).args[0] === 'No files to lint were passed. See lesshint -h');
-        console.error.restore();
+        return result.fail(function () {
+            assert(console.error.getCall(0).args[0] === 'No files to lint were passed. See lesshint -h');
+            console.error.restore();
+        });
+    });
+
+    it('should exit with a non-zero status code when lint errors were found', function () {
+        var result;
+
+        result = cli({
+            args: [path.dirname(__dirname) + '/data/files/file.less'],
+            config: path.resolve(process.cwd() + '/lib/config/defaults.json')
+        });
+
+        return result.fail(function (status) {
+            assert.ok(status === 1);
+        });
+    });
+
+    it('should exit with a non-zero status code when no files were passed', function () {
+        var result;
+
+        result = cli({
+            args: []
+        });
+
+        return result.fail(function (status) {
+            assert.ok(status === 66);
+        });
+    });
+
+    it('should exit with a non-zero status code when invalid config file is used', function () {
+        var result;
+
+        result = cli({
+            args: ['test.less'],
+            config: path.resolve(process.cwd() + '/test/data/config/invalid.json')
+        });
+
+        return result.fail(function (status) {
+            assert.ok(status === 78);
+        });
+    });
+
+    it('should exit with a zero status code when everything is alright', function () {
+        var result;
+
+        result = cli({
+            args: [path.dirname(__dirname) + '/data/files/ok.less'],
+            config: path.resolve(process.cwd() + '/test/data/config/invalid.json')
+        });
+
+        return result.fail(function (status) {
+            assert.ok(status === 78);
+        });
     });
 });
