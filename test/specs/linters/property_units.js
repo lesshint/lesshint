c70aff6d57bbd1cc1ecde876ca8fd99688f4a4c3
Jonathan Wilsson <jonathan.wilsson@gmail.com>
2015-06-24 21:04:05 +0200

    Make sure percentages are checked in propertyUnits

--- test/specs/linters/property_units.js
+++ test/specs/linters/property_units.js
@@ -181,6 +181,116 @@ describe('lesshint', function () {
             assert.deepEqual(actual, expected);
         });
 
+        it('should allow percentages when set as a allowed global unit', function () {
+            var source = '.foo { font-size: 100%; }';
+            var ast;
+
+            var options = {
+                propertyUnits: {
+                    enabled: true,
+                    global: ['%'],
+                    properties: {}
+                }
+            };
+
+            ast = linter.parseAST(source);
+            ast = ast.first().first('block').first('declaration');
+
+            assert.strictEqual(null, propertyUnits({
+                config: options,
+                node: ast
+            }));
+        });
+
+        it('should allow percentages when set as a allowed property unit', function () {
+            var source = '.foo { font-size: 100%; }';
+            var ast;
+
+            var options = {
+                propertyUnits: {
+                    enabled: true,
+                    global: [],
+                    properties: {
+                        'font-size': ['%']
+                    }
+                }
+            };
+
+            ast = linter.parseAST(source);
+            ast = ast.first().first('block').first('declaration');
+
+            assert.strictEqual(null, propertyUnits({
+                config: options,
+                node: ast
+            }));
+        });
+
+        it('should not allow percentages when set as a disallowed global unit', function () {
+            var source = '.foo { font-size: 100%; }';
+            var ast;
+
+            var expected = [{
+                column: 19,
+                file: 'test.less',
+                line: 1,
+                linter: 'propertyUnits',
+                message: 'Percentages are not allowed for "font-size".'
+            }];
+
+            var options = {
+                propertyUnits: {
+                    enabled: true,
+                    global: ['px'],
+                    properties: {}
+                }
+            };
+
+            ast = linter.parseAST(source);
+            ast = ast.first().first('block').first('declaration');
+
+            actual = propertyUnits({
+                config: options,
+                node: ast,
+                path: 'test.less'
+            });
+
+            assert.deepEqual(actual, expected);
+        });
+
+        it('should not allow percentages when set as a disallowed property unit', function () {
+            var source = '.foo { font-size: 100%; }';
+            var ast;
+
+            var expected = [{
+                column: 19,
+                file: 'test.less',
+                line: 1,
+                linter: 'propertyUnits',
+                message: 'Percentages are not allowed for "font-size".'
+            }];
+
+            var options = {
+                propertyUnits: {
+                    enabled: true,
+                    global: [],
+                    properties: {
+                        'font-size': ['px']
+                    }
+                }
+            };
+
+            ast = linter.parseAST(source);
+            ast = ast.first().first('block').first('declaration');
+
+            actual = propertyUnits({
+                config: options,
+                node: ast,
+                path: 'test.less'
+            });
+
+            assert.deepEqual(actual, expected);
+        });
+
         it('should return null when disabled', function () {
             var source = '.foo { font-size: 16px; }';
             var ast;
