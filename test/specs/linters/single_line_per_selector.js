2f65c3175ff3bd1b2b56daa4250ac8c505602bfb
Jonathan Wilsson <jonathan.wilsson@gmail.com>
2016-02-21 12:58:17 +0100

    Add 18f style option to singleLinePerSelector. Closes #117

--- test/specs/linters/single_line_per_selector.js
+++ test/specs/linters/single_line_per_selector.js
@@ -39,7 +39,7 @@ describe('lesshint', function () {
             expect(result).to.deep.equal(expected);
         });
 
-        it('should allow the comma on the new line', function () {
+        it('should allow the comma on a new line', function () {
             var source = '.foo\n,.bar {}';
             var result;
             var ast;
@@ -50,6 +50,46 @@ describe('lesshint', function () {
             result = linter.lint({}, ast);
 
             expect(result).to.be.undefined;
+        });
+
+        it('should allow selectors with short names on the same line when "style" is "18f"', function () {
+            var source = '.foo, .bar {}';
+            var result;
+            var ast;
+
+            var options = {
+                style: '18f'
+            };
+
+            ast = parseAST(source);
+            ast = ast.first('ruleset');
+
+            result = linter.lint(options, ast);
+
+            expect(result).to.be.undefined;
+        });
+
+        it('should not allow selectors with long names on the same line when "style" is "18f"', function () {
+            var source = '.foobar, .bar {}';
+            var result;
+            var ast;
+
+            var options = {
+                style: '18f'
+            };
+
+            var expected = [{
+                column: 9,
+                line: 1,
+                message: 'Each selector should be on its own line.'
+            }];
+
+            ast = parseAST(source);
+            ast = ast.first('ruleset');
+
+            result = linter.lint(options, ast);
+
+            expect(result).to.deep.equal(expected);
         });
     });
 });
