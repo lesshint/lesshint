6e59c1af89be68428a233b772724b2d0a9418a0d
Andrew Powell <powella@gilt.com>
2016-03-20 10:32:08 -0400

    comment linter now uses PostCSS

---
+++ test/specs/util.js
@@ -0,0 +1,19 @@
+
+var path = require('path');
+var getParser = require('../../lib/linter').getParser;
+
+module.exports = {
+    setup: function setup () {
+        var filename = path.basename(module.parent.filename);
+
+        return {
+            linter: require('../../lib/linters/' + filename),
+            parser: getParser,
+            parse: function (source, callback) {
+                return getParser(source).then(function (ast) {
+                    callback && callback(ast);
+                });
+            }
+        };
+    }
+};
