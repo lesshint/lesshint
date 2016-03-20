1489c1128ecedc3f075908836f61eb3986fd1601
Andrew Powell <powella@gilt.com>
2016-03-20 11:00:22 -0400

    updating borderZero specs, fixing column number

--- test/specs/linters/border_zero.js
+++ test/specs/linters/border_zero.js
@@ -1,26 +1,26 @@
 'use strict';
 
-var path = require('path');
 var expect = require('chai').expect;
-var linter = require('../../../lib/linters/' + path.basename(__filename));
-var getParser = require('../../../lib/linter').getParser;
+var spec = require('../util.js').setup();
 
 describe('lesshint', function () {
     describe('#borderZero()', function () {
+        it('should have the proper node types', function () {
+            var source = '.foo { border: none; }';
+
+            return spec.parse(source, function (ast) {
+                expect(spec.linter.nodeTypes).to.include(ast.root.first.first.type);
+            });
+        });
+
         it('should allow "none" as a value when "style" is "none" and the property is "border"', function () {
             var source = '.foo { border: none; }';
-            var result;
-            var parser;
-
             var options = {
                 style: 'none'
             };
 
-            parser = getParser(source);
-            parser.then(function (ast) {
-                var node = ast.root.nodes[0].nodes[0];
-
-                result = linter.lint(options, node);
+            return spec.parse(source, function (ast) {
+                var result = spec.linter.lint(options, ast.root.first.first);
 
                 expect(result).to.be.undefined;
             });
@@ -28,18 +28,12 @@ describe('lesshint', function () {
 
         it('should allow 0 as a value when "style" is "zero" and the property is "border"', function () {
             var source = '.foo { border: 0; }';
-            var result;
-            var parser;
-
             var options = {
                 style: 'zero'
             };
 
-            parser = getParser(source);
-            parser.then(function (ast) {
-                var node = ast.root.nodes[0].nodes[0];
-
-                result = linter.lint(options, node);
+            return spec.parse(source, function (ast) {
+                var result = spec.linter.lint(options, ast.root.first.first);
 
                 expect(result).to.be.undefined;
             });
@@ -47,9 +41,6 @@ describe('lesshint', function () {
 
         it('should not allow 0 as a value when "style" is "none" and the property is "border"', function () {
             var source = '.foo { border: 0; }';
-            var result;
-            var parser;
-
             var expected = [{
                 column: 16,
                 line: 1,
@@ -60,21 +51,15 @@ describe('lesshint', function () {
                 style: 'none'
             };
 
-            parser = getParser(source);
-            parser.then(function (ast) {
-                var node = ast.root.nodes[0].nodes[0];
-
-                result = linter.lint(options, node);
+            return spec.parse(source, function (ast) {
+                var result = spec.linter.lint(options, ast.root.first.first);
 
                 expect(result).to.deep.equal(expected);
             });
         });
-
+        //
         it('should not allow "none" as a value when "style" is "zero" and the property is "border"', function () {
             var source = '.foo { border: none; }';
-            var result;
-            var parser;
-
             var expected = [{
                 column: 16,
                 line: 1,
@@ -85,11 +70,8 @@ describe('lesshint', function () {
                 style: 'zero'
             };
 
-            parser = getParser(source);
-            parser.then(function (ast) {
-                var node = ast.root.nodes[0].nodes[0];
-
-                result = linter.lint(options, node);
+            return spec.parse(source, function (ast) {
+                var result = spec.linter.lint(options, ast.root.first.first);
 
                 expect(result).to.deep.equal(expected);
             });
@@ -97,18 +79,12 @@ describe('lesshint', function () {
 
         it('should allow "none" as a value when "style" is "none" and the property is "border-bottom"', function () {
             var source = '.foo { border-bottom: none; }';
-            var result;
-            var parser;
-
             var options = {
                 style: 'none'
             };
 
-            parser = getParser(source);
-            parser.then(function (ast) {
-                var node = ast.root.nodes[0].nodes[0];
-
-                result = linter.lint(options, node);
+            return spec.parse(source, function (ast) {
+                var result = spec.linter.lint(options, ast.root.first.first);
 
                 expect(result).to.be.undefined;
             });
@@ -116,18 +92,12 @@ describe('lesshint', function () {
 
         it('should allow "none" as a value when "style" is "none" and the property is "border-left"', function () {
             var source = '.foo { border-left: none; }';
-            var result;
-            var parser;
-
             var options = {
                 style: 'none'
             };
 
-            parser = getParser(source);
-            parser.then(function (ast) {
-                var node = ast.root.nodes[0].nodes[0];
-
-                result = linter.lint(options, node);
+            return spec.parse(source, function (ast) {
+                var result = spec.linter.lint(options, ast.root.first.first);
 
                 expect(result).to.be.undefined;
             });
@@ -135,18 +105,12 @@ describe('lesshint', function () {
 
         it('should allow "none" as a value when "style" is "none" and the property is "border-right"', function () {
             var source = '.foo { border-right: none; }';
-            var result;
-            var parser;
-
             var options = {
                 style: 'none'
             };
 
-            parser = getParser(source);
-            parser.then(function (ast) {
-                var node = ast.root.nodes[0].nodes[0];
-
-                result = linter.lint(options, node);
+            return spec.parse(source, function (ast) {
+                var result = spec.linter.lint(options, ast.root.first.first);
 
                 expect(result).to.be.undefined;
             });
@@ -154,18 +118,12 @@ describe('lesshint', function () {
 
         it('should allow "none" as a value when "style" is "none" and the property is "border-top"', function () {
             var source = '.foo { border-top: none; }';
-            var result;
-            var parser;
-
             var options = {
                 style: 'none'
             };
 
-            parser = getParser(source);
-            parser.then(function (ast) {
-                var node = ast.root.nodes[0].nodes[0];
-
-                result = linter.lint(options, node);
+            return spec.parse(source, function (ast) {
+                var result = spec.linter.lint(options, ast.root.first.first);
 
                 expect(result).to.be.undefined;
             });
@@ -173,18 +131,12 @@ describe('lesshint', function () {
 
         it('should not do anything when there is a actual border specified', function () {
             var source = '.foo { border: 1px solid #000000; }';
-            var result;
-            var parser;
-
             var options = {
                 style: 'none'
             };
 
-            parser = getParser(source);
-            parser.then(function (ast) {
-                var node = ast.root.nodes[0].nodes[0];
-
-                result = linter.lint(options, node);
+            return spec.parse(source, function (ast) {
+                var result = spec.linter.lint(options, ast.root.first.first);
 
                 expect(result).to.be.undefined;
             });
@@ -192,18 +144,13 @@ describe('lesshint', function () {
 
         it('should throw on invalid "style" value', function () {
             var source = '.foo { border: 0; }';
-            var lint;
-            var parser;
-
             var options = {
                 style: 'invalid'
             };
 
-            parser = getParser(source);
-            parser.then(function (ast) {
-                var node = ast.root.nodes[0].nodes[0];
-
-                lint = linter.lint.bind(null, options, node);
+            return spec.parse(source, function (ast) {
+                var node = ast.root.first.first;
+                var lint = spec.linter.lint.bind(null, options, node);
 
                 expect(lint).to.throw(Error);
             });
