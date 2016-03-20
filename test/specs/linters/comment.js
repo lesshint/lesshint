22808cab0ce3c2688409b8ae1276632a2bf1d94e
Andrew Powell <powella@gilt.com>
2016-03-20 10:32:08 -0400

    comment linter now uses PostCSS

--- test/specs/linters/comment.js
+++ test/specs/linters/comment.js
@@ -1,44 +1,53 @@
 'use strict';
 
-var path = require('path');
 var expect = require('chai').expect;
-var linter = require('../../../lib/linters/' + path.basename(__filename));
-var parseAST = require('../../../lib/linter').parseAST;
+var spec = require('../util.js').setup();
 
 describe('lesshint', function () {
     describe('#comment()', function () {
+        it('should have the proper node types', function () {
+            var source = '/* Hello world */';
+
+            // returning a promise allows mocha to handle the promise natively
+            return spec.parse(source, function (ast) {
+                expect(spec.linter.nodeTypes).to.include(ast.root.first.type);
+            });
+        });
+
+        it('should allow single-line comments', function () {
+            var source = '// Hello world';
+
+            return spec.parse(source, function (ast) {
+                var result = spec.linter.lint({}, ast.root.first);
+
+                expect(result).to.be.undefined;
+            });
+        });
+
         it('should not allow multi-line comments', function () {
             var source = '/* Hello world */';
-            var result;
-            var ast;
-
             var expected = [{
-                message: "There shouldn't be any multi-line comments."
+                message: 'There shouldn\'t be any multi-line comments.'
             }];
 
-            ast = parseAST(source);
-            ast = ast.first('multilineComment');
+            return spec.parse(source, function (ast) {
+                var result = spec.linter.lint({}, ast.root.first);
 
-            result = linter.lint({}, ast);
-
-            expect(result).to.deep.equal(expected);
+                expect(result).to.deep.equal(expected);
+            });
         });
 
         it('should allow comments matching "allowed" option regexp', function () {
             var source = '/*! Hello world */';
-            var result;
-            var ast;
-
             var options = {
                 allowed: '^!'
             };
 
-            ast = parseAST(source);
-            ast = ast.first('multilineComment');
+            return spec.parse(source, function (ast) {
+                var result = spec.linter.lint(options, ast.root.first);
 
-            result = linter.lint(options, ast);
-
-            expect(result).to.be.undefined;
+                expect(result).to.be.undefined;
+            });
         });
     });
 });
