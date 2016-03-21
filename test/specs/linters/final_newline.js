578d0f87e09322c3458575dad5b7644938affa24
Andrew Powell <powella@gilt.com>
2016-03-21 16:11:35 -0400

    finalNewline now uses PostCSS

--- test/specs/linters/final_newline.js
+++ test/specs/linters/final_newline.js
@@ -1,88 +1,73 @@
 'use strict';
 
-var path = require('path');
 var expect = require('chai').expect;
-var linter = require('../../../lib/linters/' + path.basename(__filename));
-var parseAST = require('../../../lib/linter').parseAST;
+var spec = require('../util.js').setup();
 
 describe('lesshint', function () {
     describe('#finalNewline()', function () {
+        it('should have the proper node types', function () {
+            var source = '.foo { }';
+
+            return spec.parse(source, function (ast) {
+                expect(spec.linter.nodeTypes).to.include(ast.root.type);
+            });
+        });
+
         it('should allow files with final new lines', function () {
             var source = '.foo {}\n';
-            var result;
-            var ast;
 
-            ast = parseAST(source);
+            return spec.parse(source, function (ast) {
+                var result = spec.linter.lint({}, ast.root);
 
-            result = linter.lint({}, ast);
-
-            expect(result).to.be.undefined;
+                expect(result).to.be.undefined;
+            });
         });
 
         it('should not allow files without final new lines', function () {
             var source = '.foo {}';
-            var result;
-            var ast;
-
             var expected = [{
                 column: 8,
                 line: 1,
                 message: 'Files should end with a newline.'
             }];
 
-            ast = parseAST(source);
+            return spec.parse(source, function (ast) {
+                var result = spec.linter.lint({}, ast.root);
 
-            result = linter.lint({}, ast);
-
-            expect(result).to.deep.equal(expected);
+                expect(result).to.deep.equal(expected);
+            });
         });
 
         it('should ignore empty files', function () {
             var source = '';
-            var result;
-            var ast;
 
-            ast = parseAST(source);
+            return spec.parse(source, function (ast) {
+                var result = spec.linter.lint({}, ast.root);
 
-            result = linter.lint({}, ast);
-
-            expect(result).to.be.undefined;
+                expect(result).to.be.undefined;
+            });
         });
 
         it('should report the correct line number even for large files. #123', function () {
-            var source = '';
-            var result;
-            var ast;
-
+            var source = '@import "something";\n';
+            var i;
             var expected = [{
                 column: 26,
-                line: 17,
+                line: 16,
                 message: 'Files should end with a newline.'
             }];
 
-            source += '@import "something";\n';
-            source += '@import "something-else";\n';
-            source += '@import "something-else";\n';
-            source += '@import "something-else";\n';
-            source += '@import "something-else";\n';
-            source += '@import "something-else";\n';
-            source += '@import "something-else";\n';
-            source += '@import "something-else";\n';
-            source += '@import "something-else";\n';
-            source += '@import "something-else";\n';
-            source += '@import "something-else";\n';
-            source += '@import "something-else";\n';
-            source += '@import "something-else";\n';
-            source += '@import "something-else";\n';
-            source += '@import "something-else";\n';
-            source += '@import "something-else";\n';
+            for (i = 0; i < 14; i++) {
+                source += '@import "something-else";\n';
+            }
+
             source += '@import "something-else";';
 
-            ast = parseAST(source);
+            return spec.parse(source, function (ast) {
+                var result = spec.linter.lint({}, ast.root);
 
-            result = linter.lint({}, ast);
-
-            expect(result).to.deep.equal(expected);
+                expect(result).to.deep.equal(expected);
+            });
         });
     });
 });
