a5192535a812eb0738923bfdc49b3e61088e457f
Andrew Powell <powella@gilt.com>
2016-03-20 22:26:39 -0400

    depthLevel now uses PostCSS

--- test/specs/linters/depth_level.js
+++ test/specs/linters/depth_level.js
@@ -1,17 +1,20 @@
 'use strict';
 
-var path = require('path');
 var expect = require('chai').expect;
-var linter = require('../../../lib/linters/' + path.basename(__filename));
-var parseAST = require('../../../lib/linter').parseAST;
+var spec = require('../util.js').setup();
 
 describe('lesshint', function () {
     describe('#depthLevel()', function () {
+        it('should have the proper node types', function () {
+            var source = '.foo { color: red; .foo-2 { color: red; .foo-3 { width: 100%; } } }';
+
+            return spec.parse(source, function (ast) {
+                expect(spec.linter.nodeTypes).to.include(ast.root.first.type);
+            });
+        });
+
         it('should not allow styles nested with more than 3 levels of depth.', function () {
             var source = '.foo { color: red; .foo-2 { color: red; .foo-3 { width: 100%; .foo-4 { height: 100%; } } } }';
-            var result;
-            var ast;
-
             var expected = [{
                 message: "There shouldn't be more than '3' levels deep from the style's parent, check the children's depth."
             }];
@@ -20,27 +23,24 @@ describe('lesshint', function () {
                 depth: 3
             };
 
-            ast = parseAST(source);
-            ast = ast.first().first('block');
+            return spec.parse(source, function (ast) {
+                var result = spec.linter.lint({}, ast.root.first);
 
-            result = linter.lint(options, ast);
-            expect(result).to.deep.equal(expected);
+                expect(result).to.deep.equal(expected);
+            });
         });
 
         it('should allow styles nested with less than 4 levels of depth.', function () {
             var source = '.foo { color: red; .foo-2 { color: red; .foo-3 { width: 100%; } } }';
-            var result;
-            var ast;
-
             var options = {
                 depth: 3
             };
 
-            ast = parseAST(source);
-            ast = ast.first().first('block');
+            return spec.parse(source, function (ast) {
+                var result = spec.linter.lint({}, ast.root.first);
 
-            result = linter.lint(options, ast);
-            expect(result).to.be.undefined;
+                expect(result).to.be.undefined;
+            });
         });
     });
 
