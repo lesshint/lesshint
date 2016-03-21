6d0f1140fcd0954848b7ec20d94d99008eddba25
Andrew Powell <powella@gilt.com>
2016-03-21 16:11:16 -0400

    fixing module parent caching in specs/util.js

--- test/specs/util.js
+++ test/specs/util.js
@@ -5,10 +5,25 @@ var getParser = require('../../lib/linter').getParser;
 
 module.exports = {
     setup: function setup () {
-        var filename = path.basename(module.parent.filename);
+        var filename = '../../lib/linters/';
+        var linter;
+
+        // slightly evil, but it's OK since this is just for specs
+        // nodejs caches module.parent as the first module to require it,
+        // which in the case of specs, is mocha.
+        // we want the linter that is requiring this, for each.
+        delete require.cache[__filename];
+
+        filename = path.join(filename, path.basename(module.parent.filename));
+
+        try {
+            linter = require(filename);
+        } catch (e) {
+            return {};
+        }
 
         return {
-            linter: require('../../lib/linters/' + filename),
+            linter: linter,
             parser: getParser,
             parse: function (source, callback) {
                 return getParser(source).then(function (ast) {
