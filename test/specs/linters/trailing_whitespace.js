e71f1e46b023fce19cb6406ae96e313ee725a625
Andrew Powell <powella@gilt.com>
2016-03-21 21:52:29 -0400

    trailingWhitespace, trailingSemicolon now use PostCSS

--- test/specs/linters/trailing_whitespace.js
+++ test/specs/linters/trailing_whitespace.js
@@ -1,82 +1,76 @@
 'use strict';
 
-var path = require('path');
 var expect = require('chai').expect;
-var linter = require('../../../lib/linters/' + path.basename(__filename));
-var parseAST = require('../../../lib/linter').parseAST;
+var spec = require('../util.js').setup();
 
 describe('lesshint', function () {
     describe('#trailingWhitespace()', function () {
+        it('should have the proper node types', function () {
+            var source = '.foo {}';
+
+            return spec.parse(source, function (ast) {
+                expect(spec.linter.nodeTypes).to.include(ast.root.type);
+            });
+        });
+
         it('should allow lines with no trailing whitespace', function () {
             var source = '.foo {}';
-            var result;
-            var ast;
 
-            ast = parseAST(source);
+            return spec.parse(source, function (ast) {
+                var result = spec.linter.lint({}, ast.root);
 
-            result = linter.lint({}, ast);
-
-            expect(result).to.be.undefined;
+                expect(result).to.be.undefined;
+            });
         });
 
         it('should allow lines with trailing new line characters', function () {
             var source = '.foo {}\n';
-            var result;
-            var ast;
 
-            ast = parseAST(source);
+            return spec.parse(source, function (ast) {
+                var result = spec.linter.lint({}, ast.root);
 
-            result = linter.lint({}, ast);
-
-            expect(result).to.be.undefined;
+                expect(result).to.be.undefined;
+            });
         });
 
         it('should not allow lines with trailing whitespace', function () {
             var source = '.foo {}  ';
-            var result;
-            var ast;
-
             var expected = [{
                 column: 9,
                 line: 1,
                 message: "There should't be any trailing whitespace."
             }];
 
-            ast = parseAST(source);
+            return spec.parse(source, function (ast) {
+                var result = spec.linter.lint({}, ast.root);
 
-            result = linter.lint({}, ast);
-
-            expect(result).to.deep.equal(expected);
+                expect(result).to.deep.equal(expected);
+            });
         });
 
         it('should not allow lines with trailing tabs', function () {
             var source = '.foo {} \t';
-            var result;
-            var ast;
-
             var expected = [{
                 column: 9,
                 line: 1,
                 message: "There should't be any trailing whitespace."
             }];
 
-            ast = parseAST(source);
+            return spec.parse(source, function (ast) {
+                var result = spec.linter.lint({}, ast.root);
 
-            result = linter.lint({}, ast);
-
-            expect(result).to.deep.equal(expected);
+                expect(result).to.deep.equal(expected);
+            });
         });
 
         it('should ignore empty files', function () {
             var source = '';
-            var result;
-            var ast;
 
-            ast = parseAST(source);
+            return spec.parse(source, function (ast) {
+                var result = spec.linter.lint({}, ast.root);
 
-            result = linter.lint({}, ast);
-
-            expect(result).to.be.undefined;
+                expect(result).to.be.undefined;
+            });
         });
     });
 });
