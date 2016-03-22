92b64a48b82aaa9a1052a2c6b661997363e0342b
Andrew Powell <powella@gilt.com>
2016-03-22 10:53:36 -0400

    hexLength now uses PostCSS

--- test/specs/linters/hex_length.js
+++ test/specs/linters/hex_length.js
@@ -1,17 +1,20 @@
 'use strict';
 
-var path = require('path');
 var expect = require('chai').expect;
-var linter = require('../../../lib/linters/' + path.basename(__filename));
-var parseAST = require('../../../lib/linter').parseAST;
+var spec = require('../util.js').setup();
 
 describe('lesshint', function () {
     describe('#hexLength()', function () {
+        it('should have the proper node types', function () {
+            var source = 'color: #ABC;';
+
+            return spec.parse(source, function (ast) {
+                expect(spec.linter.nodeTypes).to.include(ast.root.first.type);
+            });
+        });
+
         it('should not allow short hand hex values when "style" is "long"', function () {
             var source = 'color: #ABC;';
-            var result;
-            var ast;
-
             var expected = [{
                 message: '#ABC should be written in the long-form format.'
             }];
@@ -20,36 +23,28 @@ describe('lesshint', function () {
                 style: 'long'
             };
 
-            ast = parseAST(source);
-            ast = ast.first('declaration').first('value').first('color');
+            return spec.parse(source, function (ast) {
+                var result = spec.linter.lint(options, ast.root.first);
 
-            result = linter.lint(options, ast);
-
-            expect(result).to.deep.equal(expected);
+                expect(result).to.deep.equal(expected);
+            });
         });
 
         it('should allow longhand hex values when "style" is "long"', function () {
             var source = 'color: #AABBCC;';
-            var result;
-            var ast;
-
             var options = {
                 style: 'long'
             };
 
-            ast = parseAST(source);
-            ast = ast.first('declaration').first('value').first('color');
+            return spec.parse(source, function (ast) {
+                var result = spec.linter.lint(options, ast.root.first);
 
-            result = linter.lint(options, ast);
-
-            expect(result).to.be.undefined;
+                expect(result).to.be.undefined;
+            });
         });
 
         it('should not allow longhand hex values when "style" is "short"', function () {
             var source = 'color: #AABBCC;';
-            var result;
-            var ast;
-
             var expected = [{
                 message: '#AABBCC should be written in the short-form format.'
             }];
@@ -58,53 +53,41 @@ describe('lesshint', function () {
                 style: 'short'
             };
 
-            ast = parseAST(source);
-            ast = ast.first('declaration').first('value').first('color');
+            return spec.parse(source, function (ast) {
+                var result = spec.linter.lint(options, ast.root.first);
 
-            result = linter.lint(options, ast);
-
-            expect(result).to.deep.equal(expected);
+                expect(result).to.deep.equal(expected);
+            });
         });
 
         it('should allow short hand hex values when "style" is "short"', function () {
             var source = 'color: #ABC;';
-            var result;
-            var ast;
-
             var options = {
                 style: 'short'
             };
 
-            ast = parseAST(source);
-            ast = ast.first('declaration').first('value').first('color');
+            return spec.parse(source, function (ast) {
+                var result = spec.linter.lint(options, ast.root.first);
 
-            result = linter.lint(options, ast);
-
-            expect(result).to.be.undefined;
+                expect(result).to.be.undefined;
+            });
         });
 
         it('should allow longhand hex values can can be written with a shorthand when "style" is "short"', function () {
             var source = 'color: #4B7A19;';
-            var result;
-            var ast;
-
             var options = {
                 style: 'short'
             };
 
-            ast = parseAST(source);
-            ast = ast.first('declaration').first('value').first('color');
+            return spec.parse(source, function (ast) {
+                var result = spec.linter.lint(options, ast.root.first);
 
-            result = linter.lint(options, ast);
-
-            expect(result).to.be.undefined;
+                expect(result).to.be.undefined;
+            });
         });
 
         it('should find hex values in background declarations', function () {
             var source = 'background: url(test.png) no-repeat #AABBCC;';
-            var result;
-            var ast;
-
             var expected = [{
                 message: '#AABBCC should be written in the short-form format.'
             }];
@@ -113,19 +96,15 @@ describe('lesshint', function () {
                 style: 'short'
             };
 
-            ast = parseAST(source);
-            ast = ast.first('declaration').first('value').first('color');
+            return spec.parse(source, function (ast) {
+                var result = spec.linter.lint(options, ast.root.first);
 
-            result = linter.lint(options, ast);
-
-            expect(result).to.deep.equal(expected);
+                expect(result).to.deep.equal(expected);
+            });
         });
 
         it('should not allow short hand hex values in variables when "style" is "long" (#28)', function () {
             var source = '@color: #ABC;';
-            var result;
-            var ast;
-
             var expected = [{
                 message: '#ABC should be written in the long-form format.'
             }];
@@ -134,19 +113,15 @@ describe('lesshint', function () {
                 style: 'long'
             };
 
-            ast = parseAST(source);
-            ast = ast.first().first('color');
+            return spec.parse(source, function (ast) {
+                var result = spec.linter.lint(options, ast.root.first);
 
-            result = linter.lint(options, ast);
-
-            expect(result).to.deep.equal(expected);
+                expect(result).to.deep.equal(expected);
+            });
         });
 
         it('should not allow longhand hex values in variables when "style" is "short" (#28)', function () {
             var source = '@color: #AABBCC;';
-            var result;
-            var ast;
-
             var expected = [{
                 message: '#AABBCC should be written in the short-form format.'
             }];
@@ -155,46 +130,37 @@ describe('lesshint', function () {
                 style: 'short'
             };
 
-            ast = parseAST(source);
-            ast = ast.first().first('color');
+            return spec.parse(source, function (ast) {
+                var result = spec.linter.lint(options, ast.root.first);
 
-            result = linter.lint(options, ast);
-
-            expect(result).to.deep.equal(expected);
+                expect(result).to.deep.equal(expected);
+            });
         });
 
         it('should ignore invalid colors', function () {
             var source = 'color: #abc1;';
-            var result;
-            var ast;
-
             var options = {
                 style: 'long'
             };
 
-            ast = parseAST(source);
-            ast = ast.first('declaration').first('value').first('color');
+            return spec.parse(source, function (ast) {
+                var result = spec.linter.lint(options, ast.root.first);
 
-            result = linter.lint(options, ast);
-
-            expect(result).to.be.undefined;
+                expect(result).to.be.undefined;
+            });
         });
 
         it('should throw on invalid "style" value', function () {
             var source = 'color: #aabbcc;';
-            var lint;
-            var ast;
-
             var options = {
                 style: 'invalid'
             };
 
-            ast = parseAST(source);
-            ast = ast.first('declaration').first('value').first('color');
+            return spec.parse(source, function (ast) {
+                var lint = spec.linter.lint.bind(null, options, ast.root.first);
 
-            lint = linter.lint.bind(null, options, ast);
-
-            expect(lint).to.throw(Error);
+                expect(lint).to.throw(Error);
+            });
         });
     });
 });
