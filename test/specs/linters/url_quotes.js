288c96b5db57cf12fb8eef984510badb9d661cfd
Andrew Powell <powella@gilt.com>
2016-03-21 22:50:41 -0400

    urlQuotes now uses PostCSS

--- test/specs/linters/url_quotes.js
+++ test/specs/linters/url_quotes.js
@@ -1,119 +1,109 @@
 'use strict';
 
-var path = require('path');
 var expect = require('chai').expect;
-var linter = require('../../../lib/linters/' + path.basename(__filename));
-var parseAST = require('../../../lib/linter').parseAST;
+var spec = require('../util.js').setup();
 
 describe('lesshint', function () {
     describe('#urlQuotes()', function () {
+        it('should have the proper node types', function () {
+            var source = ".foo { background-image: url('img/image.jpg'); }";
+
+            return spec.parse(source, function (ast) {
+                expect(spec.linter.nodeTypes).to.include(ast.root.first.first.type);
+            });
+        });
+
+        it('should have the proper node types', function () {
+            var source = '@import url(http://example.com)';
+
+            return spec.parse(source, function (ast) {
+                expect(spec.linter.nodeTypes).to.include(ast.root.first.type);
+            });
+        });
+
         it('should allow single quotes', function () {
             var source = ".foo { background-image: url('img/image.jpg'); }";
-            var result;
-            var ast;
 
-            ast = parseAST(source);
-            ast = ast.first().first('block').first('declaration');
+            return spec.parse(source, function (ast) {
+                var result = spec.linter.lint({}, ast.root.first.first);
 
-            result = linter.lint({}, ast);
-
-            expect(result).to.be.undefined;
+                expect(result).to.be.undefined;
+            });
         });
 
         it('should allow double quotes', function () {
             var source = '.foo { background-image: url("img/image.jpg"); }';
-            var result;
-            var ast;
 
-            ast = parseAST(source);
-            ast = ast.first().first('block').first('declaration');
+            return spec.parse(source, function (ast) {
+                var result = spec.linter.lint({}, ast.root.first.first);
 
-            result = linter.lint({}, ast);
-
-            expect(result).to.be.undefined;
+                expect(result).to.be.undefined;
+            });
         });
 
         it('should not allow missing quotes', function () {
             var source = '.foo { background-image: url(img/image.jpg); }';
-            var result;
-            var ast;
-
             var expected = [{
-                column: 26,
+                column: 8,
                 line: 1,
                 message: 'URLs should be enclosed in quotes.'
             }];
 
-            ast = parseAST(source);
-            ast = ast.first().first('block').first('declaration');
+            return spec.parse(source, function (ast) {
+                var result = spec.linter.lint({}, ast.root.first.first);
 
-            result = linter.lint({}, ast);
-
-            expect(result).to.deep.equal(expected);
+                expect(result).to.deep.equal(expected);
+            });
         });
 
         it('should not allow missing quotes in imports', function () {
             var source = '@import url(http://example.com)';
-            var result;
-            var ast;
-
             var expected = [{
-                column: 9,
+                column: 1,
                 line: 1,
                 message: 'URLs should be enclosed in quotes.'
             }];
 
-            ast = parseAST(source);
-            ast = ast.first('atrule');
+            return spec.parse(source, function (ast) {
+                var result = spec.linter.lint({}, ast.root.first);
 
-            result = linter.lint({}, ast);
-
-            expect(result).to.deep.equal(expected);
+                expect(result).to.deep.equal(expected);
+            });
         });
 
         it('should allow quoted urls in imports', function () {
             var source = "@import url('http://example.com')";
-            var result;
-            var ast;
 
-            ast = parseAST(source);
-            ast = ast.first('atrule');
+            return spec.parse(source, function (ast) {
+                var result = spec.linter.lint({}, ast.root.first);
 
-            result = linter.lint({}, ast);
-
-            expect(result).to.be.undefined;
+                expect(result).to.be.undefined;
+            });
         });
 
         it('should allow quoted URLs strings surrounded by spaces (#22)', function () {
             var source = ".foo { background-image: url( 'img/image.jpg' ); }";
-            var result;
-            var ast;
 
-            ast = parseAST(source);
-            ast = ast.first().first('block').first('declaration');
+            return spec.parse(source, function (ast) {
+                var result = spec.linter.lint({}, ast.root.first.first);
 
-            result = linter.lint({}, ast);
-
-            expect(result).to.be.undefined;
+                expect(result).to.be.undefined;
+            });
         });
 
         it('should not allow unquoted URLs strings surrounded by spaces (#22)', function () {
             var source = '.foo { background-image: url( img/image.jpg ); }';
-            var result;
-            var ast;
-
             var expected = [{
-                column: 26,
+                column: 8,
                 line: 1,
                 message: 'URLs should be enclosed in quotes.'
             }];
 
-            ast = parseAST(source);
-            ast = ast.first().first('block').first('declaration');
+            return spec.parse(source, function (ast) {
+                var result = spec.linter.lint({}, ast.root.first.first);
 
-            result = linter.lint({}, ast);
-
-            expect(result).to.deep.equal(expected);
+                expect(result).to.deep.equal(expected);
+            });
         });
     });
 });
