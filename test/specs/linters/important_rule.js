ee85d37273ffed1fa6ab6f44eba48862976bd6f8
Andrew Powell <powella@gilt.com>
2016-03-21 20:18:38 -0400

    importantRule now uses PostCSS

--- test/specs/linters/important_rule.js
+++ test/specs/linters/important_rule.js
@@ -1,42 +1,41 @@
 'use strict';
 
-var path = require('path');
 var expect = require('chai').expect;
-var linter = require('../../../lib/linters/' + path.basename(__filename));
-var parseAST = require('../../../lib/linter').parseAST;
+var spec = require('../util.js').setup();
 
 describe('lesshint', function () {
     describe('#importantRule()', function () {
+        it('should have the proper node types', function () {
+            var source = '.foo { color: red; }';
+
+            return spec.parse(source, function (ast) {
+                expect(spec.linter.nodeTypes).to.include(ast.root.first.type);
+            });
+        });
+
         it('should not do anything when there is no !important present', function () {
             var source = '.foo { color: red; }';
-            var result;
-            var ast;
 
-            ast = parseAST(source);
-            ast = ast.first().first('block').first('declaration');
+            return spec.parse(source, function (ast) {
+                var result = spec.linter.lint({}, ast.root.first);
 
-            result = linter.lint({}, ast);
-
-            expect(result).to.be.undefined;
+                expect(result).to.be.undefined;
+            });
         });
 
         it('should not allow !important', function () {
             var source = '.foo { color: red !important; }';
-            var result;
-            var ast;
-
             var expected = [{
-                column: 19,
+                column: 8,
                 line: 1,
                 message: '!important should not be used.'
             }];
 
-            ast = parseAST(source);
-            ast = ast.first().first('block').first('declaration');
+            return spec.parse(source, function (ast) {
+                var result = spec.linter.lint({}, ast.root.first);
 
-            result = linter.lint({}, ast);
-
-            expect(result).to.deep.equal(expected);
+                expect(result).to.deep.equal(expected);
+            });
         });
     });
 });
