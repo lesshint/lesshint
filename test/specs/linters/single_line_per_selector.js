bfbbc4e1d8ab327b8d83ea01c3d804e97a9082e4
Andrew Powell <powella@gilt.com>
2016-03-23 01:49:26 -0400

    singleLinePerSelector now uses PostCSS

--- test/specs/linters/single_line_per_selector.js
+++ test/specs/linters/single_line_per_selector.js
@@ -1,95 +1,90 @@
 'use strict';
 
-var path = require('path');
 var expect = require('chai').expect;
-var linter = require('../../../lib/linters/' + path.basename(__filename));
-var parseAST = require('../../../lib/linter').parseAST;
+var spec = require('../util.js').setup();
 
 describe('lesshint', function () {
     describe('#singleLinePerSelector()', function () {
+        it('should have the proper node types', function () {
+            var source = '.foo {}';
+
+            return spec.parse(source, function (ast) {
+                expect(spec.linter.nodeTypes).to.include(ast.root.first.type);
+            });
+        });
+
         it('should allow selectors on separate lines', function () {
             var source = '.foo, \n.bar {}';
-            var result;
-            var ast;
 
-            ast = parseAST(source);
-            ast = ast.first('ruleset');
+            return spec.parse(source, function (ast) {
+                var result = spec.linter.lint({}, ast.root.first);
 
-            result = linter.lint({}, ast);
-
-            expect(result).to.be.undefined;
+                expect(result).to.be.undefined;
+            });
         });
 
         it('should not allow multiple selectors on the same line', function () {
             var source = '.foo, .bar {}';
-            var result;
-            var ast;
+            var expected = [
+                {
+                    column: 1,
+                    line: 1,
+                    message: 'Each selector should be on its own line.'
+                },
+                {
+                    column: 7,
+                    line: 1,
+                    message: 'Each selector should be on its own line.'
+                }
+            ];
 
-            var expected = [{
-                column: 6,
-                line: 1,
-                message: 'Each selector should be on its own line.'
-            }];
+            return spec.parse(source, function (ast) {
+                var result = spec.linter.lint({}, ast.root.first);
 
-            ast = parseAST(source);
-            ast = ast.first('ruleset');
-
-            result = linter.lint({}, ast);
-
-            expect(result).to.deep.equal(expected);
+                expect(result).to.deep.equal(expected);
+            });
         });
 
         it('should allow the comma on a new line', function () {
             var source = '.foo\n,.bar {}';
-            var result;
-            var ast;
 
-            ast = parseAST(source);
-            ast = ast.first('ruleset');
+            return spec.parse(source, function (ast) {
+                var result = spec.linter.lint({}, ast.root.first);
 
-            result = linter.lint({}, ast);
-
-            expect(result).to.be.undefined;
+                expect(result).to.be.undefined;
+            });
         });
 
         it('should allow selectors with short names on the same line when "style" is "18f"', function () {
             var source = '.foo, .bar {}';
-            var result;
-            var ast;
-
             var options = {
                 style: '18f'
             };
 
-            ast = parseAST(source);
-            ast = ast.first('ruleset');
+            return spec.parse(source, function (ast) {
+                var result = spec.linter.lint(options, ast.root.first);
 
-            result = linter.lint(options, ast);
-
-            expect(result).to.be.undefined;
+                expect(result).to.be.undefined;
+            });
         });
 
         it('should not allow selectors with long names on the same line when "style" is "18f"', function () {
             var source = '.foobar, .bar {}';
-            var result;
-            var ast;
-
             var options = {
                 style: '18f'
             };
 
             var expected = [{
-                column: 9,
+                column: 10,
                 line: 1,
                 message: 'Each selector should be on its own line.'
             }];
 
-            ast = parseAST(source);
-            ast = ast.first('ruleset');
+            return spec.parse(source, function (ast) {
+                var result = spec.linter.lint(options, ast.root.first);
 
-            result = linter.lint(options, ast);
-
-            expect(result).to.deep.equal(expected);
+                expect(result).to.deep.equal(expected);
+            });
         });
     });
 });
