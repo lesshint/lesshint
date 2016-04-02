897de32fc7b7f8bea8503b3eba3da611b676e69a
Jonathan Wilsson <jonathan.wilsson@gmail.com>
2016-04-02 13:30:51 +0200

    zeroUnit now uses PostCSS

--- test/specs/linters/zero_unit.js
+++ test/specs/linters/zero_unit.js
@@ -1,19 +1,22 @@
 'use strict';
 
-var path = require('path');
 var expect = require('chai').expect;
-var linter = require('../../../lib/linters/' + path.basename(__filename));
-var parseAST = require('../../../lib/linter').parseAST;
+var spec = require('../util.js').setup();
 
 describe('lesshint', function () {
     describe('#zeroUnit()', function () {
-        it('should report units on zero values when "style" is "no_unit"', function () {
-            var source = '.foo { margin-right: 0px; }';
-            var result;
-            var ast;
+        it('should have the proper node types', function () {
+            var source = 'margin-right: 0px;';
 
+            return spec.parse(source, function (ast) {
+                expect(spec.linter.nodeTypes).to.include(ast.root.first.type);
+            });
+        });
+
+        it('should report units on zero values when "style" is "no_unit"', function () {
+            var source = 'margin-right: 0px;';
             var expected = [{
-                column: 22,
+                column: 15,
                 line: 1,
                 message: 'Unit should be omitted on zero values.'
             }];
@@ -22,55 +25,43 @@ describe('lesshint', function () {
                 style: 'no_unit'
             };
 
-            ast = parseAST(source);
-            ast = ast.first().first('block').first('declaration');
+            return spec.parse(source, function (ast) {
+                var result = spec.linter.lint(options, ast.root.first);
 
-            result = linter.lint(options, ast);
-
-            expect(result).to.deep.equal(expected);
+                expect(result).to.deep.equal(expected);
+            });
         });
 
         it('should not report anything on zero values without units when "style" is "no_unit"', function () {
-            var source = '.foo { margin-right: 0; }';
-            var result;
-            var ast;
-
+            var source = 'margin-right: 0;';
             var options = {
                 style: 'no_unit'
             };
 
-            ast = parseAST(source);
-            ast = ast.first().first('block').first('declaration');
+            return spec.parse(source, function (ast) {
+                var result = spec.linter.lint(options, ast.root.first);
 
-            result = linter.lint(options, ast);
-
-            expect(result).to.be.undefined;
+                expect(result).to.be.undefined;
+            });
         });
 
         it('should not report units on zero values when "style" is "keep_unit"', function () {
-            var source = '.foo { margin-right: 0px; }';
-            var result;
-            var ast;
-
+            var source = 'margin-right: 0px;';
             var options = {
                 style: 'keep_unit'
             };
 
-            ast = parseAST(source);
-            ast = ast.first().first('block').first('declaration');
+            return spec.parse(source, function (ast) {
+                var result = spec.linter.lint(options, ast.root.first);
 
-            result = linter.lint(options, ast);
-
-            expect(result).to.be.undefined;
+                expect(result).to.be.undefined;
+            });
         });
 
         it('should report missing units on zero values when "style" is "keep_unit"', function () {
-            var source = '.foo { margin-right: 0; }';
-            var result;
-            var ast;
-
+            var source = 'margin-right: 0;';
             var expected = [{
-                column: 22,
+                column: 15,
                 line: 1,
                 message: 'Unit should not be omitted on zero values.'
             }];
@@ -79,116 +70,91 @@ describe('lesshint', function () {
                 style: 'keep_unit'
             };
 
-            ast = parseAST(source);
-            ast = ast.first().first('block').first('declaration');
+            return spec.parse(source, function (ast) {
+                var result = spec.linter.lint(options, ast.root.first);
 
-            result = linter.lint(options, ast);
-
-            expect(result).to.deep.equal(expected);
+                expect(result).to.deep.equal(expected);
+            });
         });
 
         it('should not report units on zero values when the unit is an angle and "style" is "no_unit"', function () {
-            var source = '.foo { transform: rotate(90deg); }';
-            var result;
-            var ast;
-
+            var source = 'transform: rotate(90deg);';
             var options = {
                 style: 'no_unit'
             };
 
-            ast = parseAST(source);
-            ast = ast.first().first('block').first('declaration');
+            return spec.parse(source, function (ast) {
+                var result = spec.linter.lint(options, ast.root.first);
 
-            result = linter.lint(options, ast);
-
-            expect(result).to.be.undefined;
+                expect(result).to.be.undefined;
+            });
         });
 
         it('should not report units on zero values when the unit is a time and "style" is "no_unit"', function () {
-            var source = '.foo { transition: all 0s; }';
-            var result;
-            var ast;
-
+            var source = 'transition: all 0s;';
             var options = {
                 style: 'no_unit'
             };
 
-            ast = parseAST(source);
-            ast = ast.first().first('block').first('declaration');
+            return spec.parse(source, function (ast) {
+                var result = spec.linter.lint(options, ast.root.first);
 
-            result = linter.lint(options, ast);
-
-            expect(result).to.be.undefined;
+                expect(result).to.be.undefined;
+            });
         });
 
         it('should not report units on zero values when the unit is configured and "style" is "no_unit"', function () {
-            var source = '.foo { margin-left: 0zz; }';
-            var result;
-            var ast;
-
+            var source = 'margin-left: 0zz;';
             var options = {
                 style: 'no_unit',
                 units: ['zz']
             };
 
-            ast = parseAST(source);
-            ast = ast.first().first('block').first('declaration');
+            return spec.parse(source, function (ast) {
+                var result = spec.linter.lint(options, ast.root.first);
 
-            result = linter.lint(options, ast);
-
-            expect(result).to.be.undefined;
+                expect(result).to.be.undefined;
+            });
         });
 
         it('should not report units on zero values when the the property does not have units and "style" is "no_unit"', function () {
-            var source = '.bar { z-index: 0; }';
-            var result;
-            var ast;
-
+            var source = 'z-index: 0;';
             var options = {
                 style: 'no_unit'
             };
 
-            ast = parseAST(source);
-            ast = ast.first().first('block').first('declaration');
+            return spec.parse(source, function (ast) {
+                var result = spec.linter.lint(options, ast.root.first);
 
-            result = linter.lint(options, ast);
-
-            expect(result).to.be.undefined;
+                expect(result).to.be.undefined;
+            });
         });
 
         it('should not report units on zero values when the the property does not have units and "style" is "no_unit"', function () {
-            var source = '.bar { margin-left: 0; }';
-            var result;
-            var ast;
-
+            var source = 'margin-left: 0;';
             var options = {
                 style: 'no_unit',
                 exclude: ['margin-left']
             };
 
-            ast = parseAST(source);
-            ast = ast.first().first('block').first('declaration');
+            return spec.parse(source, function (ast) {
+                var result = spec.linter.lint(options, ast.root.first);
 
-            result = linter.lint(options, ast);
-
-            expect(result).to.be.undefined;
+                expect(result).to.be.undefined;
+            });
         });
 
         it('should throw on invalid "style" value', function () {
-            var source = '.foo { margin-right: 0; }';
-            var lint;
-            var ast;
-
+            var source = 'margin-right: 0;';
             var options = {
                 style: 'invalid'
             };
 
-            ast = parseAST(source);
-            ast = ast.first().first('block').first('declaration');
+            return spec.parse(source, function (ast) {
+                var lint = spec.linter.lint.bind(null, options, ast.root.first);
 
-            lint = linter.lint.bind(null, options, ast);
-
-            expect(lint).to.throw(Error);
+                expect(lint).to.throw(Error);
+            });
         });
     });
 });
