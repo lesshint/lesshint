14903609af186de814a2f05232ae79c4d22fd107
Jonathan Wilsson <jonathan.wilsson@gmail.com>
2016-04-20 12:10:13 +0200

    qualifyingElement now uses PostCSS

--- test/specs/linters/qualifying_element.js
+++ test/specs/linters/qualifying_element.js
@@ -1,169 +1,140 @@
 'use strict';
 
-var path = require('path');
 var expect = require('chai').expect;
-var linter = require('../../../lib/linters/' + path.basename(__filename));
-var parseAST = require('../../../lib/linter').parseAST;
+var spec = require('../util.js').setup();
 
 describe('lesshint', function () {
     describe('#qualifyingElement()', function () {
+        it('should have the proper node types', function () {
+            var source = 'input[type="text"] {}';
+
+            return spec.parse(source, function (ast) {
+                expect(spec.linter.nodeTypes).to.include(ast.root.first.type);
+            });
+        });
+
         it('should allow selectors without any qualifying element', function () {
             var source = '.foo {}';
-            var result;
-            var ast;
 
-            ast = parseAST(source);
-            ast = ast.first().first('selector');
+            return spec.parse(source, function (ast) {
+                var result = spec.linter.lint({}, ast.root.first);
 
-            result = linter.lint({}, ast);
-
-            expect(result).to.be.undefined;
+                expect(result).to.be.undefined;
+            });
         });
 
         it('should not allow ID selector with a qualifying element', function () {
             var source = 'div#foo {}';
-            var result;
-            var ast;
-
             var expected = [{
                 column: 4,
                 line: 1,
                 message: 'Id selectors should not include a qualifying element.'
             }];
 
-            ast = parseAST(source);
-            ast = ast.first().first('selector');
+            return spec.parse(source, function (ast) {
+                var result = spec.linter.lint({}, ast.root.first);
 
-            result = linter.lint({}, ast);
-
-            expect(result).to.deep.equal(expected);
+                expect(result).to.deep.equal(expected);
+            });
         });
 
         it('should not allow class selector with a qualifying element', function () {
             var source = 'div.foo {}';
-            var result;
-            var ast;
-
             var expected = [{
                 column: 4,
                 line: 1,
                 message: 'Class selectors should not include a qualifying element.'
             }];
 
-            ast = parseAST(source);
-            ast = ast.first().first('selector');
+            return spec.parse(source, function (ast) {
+                var result = spec.linter.lint({}, ast.root.first);
 
-            result = linter.lint({}, ast);
-
-            expect(result).to.deep.equal(expected);
+                expect(result).to.deep.equal(expected);
+            });
         });
 
         it('should not allow attribute selector with a qualifying element', function () {
             var source = 'div[foo="bar"] {}';
-            var result;
-            var ast;
-
             var expected = [{
                 column: 4,
                 line: 1,
                 message: 'Attribute selectors should not include a qualifying element.'
             }];
 
-            ast = parseAST(source);
-            ast = ast.first().first('selector');
+            return spec.parse(source, function (ast) {
+                var result = spec.linter.lint({}, ast.root.first);
 
-            result = linter.lint({}, ast);
-
-            expect(result).to.deep.equal(expected);
+                expect(result).to.deep.equal(expected);
+            });
         });
 
         it('should allow ID selector with a qualifying element when "allowWithId" is "true"', function () {
             var source = 'div#foo {}';
-            var result;
-            var ast;
-
             var options = {
                 allowWithId: true
             };
 
-            ast = parseAST(source);
-            ast = ast.first().first('selector');
+            return spec.parse(source, function (ast) {
+                var result = spec.linter.lint(options, ast.root.first);
 
-            result = linter.lint(options, ast);
-
-            expect(result).to.be.undefined;
+                expect(result).to.be.undefined;
+            });
         });
 
         it('should allow class selector with a qualifying element when "allowWithClass" is "true"', function () {
             var source = 'div.foo {}';
-            var result;
-            var ast;
-
             var options = {
                 allowWithClass: true
             };
 
-            ast = parseAST(source);
-            ast = ast.first().first('selector');
+            return spec.parse(source, function (ast) {
+                var result = spec.linter.lint(options, ast.root.first);
 
-            result = linter.lint(options, ast);
-
-            expect(result).to.be.undefined;
+                expect(result).to.be.undefined;
+            });
         });
 
         it('should allow attribute selector with a qualifying element when "allowWithAttribute" is "true"', function () {
             var source = 'div[foo="bar"] {}';
-            var result;
-            var ast;
-
             var options = {
                 allowWithAttribute: true
             };
 
-            ast = parseAST(source);
-            ast = ast.first().first('selector');
+            return spec.parse(source, function (ast) {
+                var result = spec.linter.lint(options, ast.root.first);
 
-            result = linter.lint(options, ast);
-
-            expect(result).to.be.undefined;
+                expect(result).to.be.undefined;
+            });
         });
 
         it('should not allow a class with a qualifying element in a descendant selector', function () {
             var source = '.foo div.bar {}';
-            var result;
-            var ast;
-
             var expected = [{
                 column: 9,
                 line: 1,
                 message: 'Class selectors should not include a qualifying element.'
             }];
 
-            ast = parseAST(source);
-            ast = ast.first().first('selector');
+            return spec.parse(source, function (ast) {
+                var result = spec.linter.lint({}, ast.root.first);
 
-            result = linter.lint({}, ast);
-
-            expect(result).to.deep.equal(expected);
+                expect(result).to.deep.equal(expected);
+            });
         });
 
         it('should check parent selectors. #106', function () {
             var source = 'a { &.active { color: red; } }';
-            var result;
-            var ast;
-
             var expected = [{
-                column: 6,
+                column: 2,
                 line: 1,
                 message: 'Class selectors should not include a qualifying element.'
             }];
 
-            ast = parseAST(source);
-            ast = ast.first().first('block').first('ruleset').first('selector');
+            return spec.parse(source, function (ast) {
+                var result = spec.linter.lint({}, ast.root.first.first);
 
-            result = linter.lint({}, ast);
-
-            expect(result).to.deep.equal(expected);
+                expect(result).to.deep.equal(expected);
+            });
         });
     });
 });
