3d7cee12d9adf4bef2143b5f49ed0878baadf11e
Jonathan Wilsson <jonathan.wilsson@gmail.com>
2016-04-22 10:21:06 +0200

    spaceAroundOperator now uses PostCSS

--- test/specs/linters/space_around_operator.js
+++ test/specs/linters/space_around_operator.js
@@ -1,299 +1,207 @@
 'use strict';
 
-var path = require('path');
 var expect = require('chai').expect;
-var linter = require('../../../lib/linters/' + path.basename(__filename));
-var parseAST = require('../../../lib/linter').parseAST;
+var spec = require('../util.js').setup();
 
 describe('lesshint', function () {
     describe('#spaceAroundOperator', function () {
-        it('should allow one space before and after operator when "style" is "both"', function () {
-            var source = '.foo { height: calc(10px + 10px); }';
-            var result;
-            var ast;
+        var options;
 
-            var options = {
-                style: 'both'
-            };
+        it('should have the proper node types', function () {
+            var source = 'height: calc(10px + 10px);';
 
-            ast = parseAST(source);
-            ast = ast.first();
-
-            result = linter.lint(options, ast);
-
-            expect(result).to.be.undefined;
-        });
-
-        it('should not allow missing space before operator when "style" is "both"', function () {
-            var source = '.foo { height: calc(10px+ 10px); }';
-            var result;
-            var ast;
-
-            var expected = [{
-                column: 21,
-                line: 1,
-                message: 'Operators should be preceded and followed by one space.'
-            }];
-
-            var options = {
-                style: 'both'
-            };
-
-            ast = parseAST(source);
-            ast = ast.first();
-
-            result = linter.lint(options, ast);
-
-            expect(result).to.deep.equal(expected);
-        });
-
-        it('should not allow missing space after operator when "style" is "both"', function () {
-            var source = '.foo { height: calc(10px +10px); }';
-            var result;
-            var ast;
-
-            var expected = [{
-                column: 27,
-                line: 1,
-                message: 'Operators should be preceded and followed by one space.'
-            }];
-
-            var options = {
-                style: 'both'
-            };
-
-            ast = parseAST(source);
-            ast = ast.first();
-
-            result = linter.lint(options, ast);
-
-            expect(result).to.deep.equal(expected);
-        });
-
-        it('should allow a missing space before operator when "style" is "none"', function () {
-            var source = '.foo { height: calc(10px+10px); }';
-            var result;
-            var ast;
-
-            var options = {
-                style: 'none'
-            };
-
-            ast = parseAST(source);
-            ast = ast.first();
-
-            result = linter.lint(options, ast);
-
-            expect(result).to.be.undefined;
-        });
-
-        it('should not allow one space before operator when "style" is "none"', function () {
-            var source = '.foo { height: calc(10px + 10px); }';
-            var result;
-            var ast;
-
-            var expected = [{
-                column: 25,
-                line: 1,
-                message: 'Operators should not be preceded nor followed by any space.'
-            }];
-
-            var options = {
-                style: 'none'
-            };
-
-            ast = parseAST(source);
-            ast = ast.first();
-
-            result = linter.lint(options, ast);
-
-            expect(result).to.deep.equal(expected);
-        });
-
-        it('should allow a missing space after operator when "style" is "none"', function () {
-            var source = '.foo { height: calc(10px+10px); }';
-            var result;
-            var ast;
-
-            var options = {
-                style: 'none'
-            };
-
-            ast = parseAST(source);
-            ast = ast.first();
-
-            result = linter.lint(options, ast);
-
-            expect(result).to.be.undefined;
-        });
-
-        it('should not allow one space after operator when "style" is "none"', function () {
-            var source = '.foo { height: calc(10px+ 10px); }';
-            var result;
-            var ast;
-
-            var expected = [{
-                column: 26,
-                line: 1,
-                message: 'Operators should not be preceded nor followed by any space.'
-            }];
-
-            var options = {
-                style: 'none'
-            };
-
-            ast = parseAST(source);
-            ast = ast.first();
-
-            result = linter.lint(options, ast);
-
-            expect(result).to.deep.equal(expected);
-        });
-
-        it('should not report on commas', function () {
-            var source = '@var: rgb(255,255,255);';
-            var result;
-            var ast;
-
-            var options = {
-                style: 'both'
-            };
-
-            ast = parseAST(source);
-            ast = ast.first();
-
-            result = linter.lint(options, ast);
-
-            expect(result).to.be.undefined;
-        });
-
-        describe('negative numbers when calculating', function () {
-            it('should not report on negative number in the end of whole value', function () {
-                var source = '@foo: (@bar / 3) * -1;';
-                var result;
-                var ast;
-
-                var options = {
-                    style: 'both'
-                };
-
-                ast = parseAST(source);
-                ast = ast.first();
-
-                result = linter.lint(options, ast);
-
-                expect(result).to.be.undefined;
-            });
-
-            it('should not report on negative number in the end of brackets', function () {
-                var source = '@b: spin(@a, -2%);';
-                var result;
-                var ast;
-
-                var options = {
-                    style: 'both'
-                };
-
-                ast = parseAST(source);
-                ast = ast.first();
-
-                result = linter.lint(options, ast);
-
-                expect(result).to.be.undefined;
+            return spec.parse(source, function (ast) {
+                expect(spec.linter.nodeTypes).to.include(ast.root.first.type);
             });
         });
 
-        it('should not report on minus values', function () {
-            var source = '.foo { margin-left: -10px; }';
-            var result;
-            var ast;
+        describe('when "style" is "both"', function () {
+            beforeEach(function () {
+                options = {
+                    style: 'both'
+                };
+            });
 
-            var options = {
-                style: 'both'
-            };
+            it('should allow one space before and after operator', function () {
+                var source = 'height: calc(10px + 10px);';
 
-            ast = parseAST(source);
-            ast = ast.first();
+                return spec.parse(source, function (ast) {
+                    var result = spec.linter.lint(options, ast.root.first);
 
-            result = linter.lint(options, ast);
+                    expect(result).to.be.undefined;
+                });
+            });
 
-            expect(result).to.be.undefined;
-        });
-
-        describe('shorthands', function () {
-            it('should not allow when there is space after operator', function () {
-                var source = '.foo { margin: 10px - 1px 0px 20px; }';
-                var result;
-                var ast;
-
+            it('should not allow missing space before operator', function () {
+                var source = 'height: calc(10px+ 10px);';
                 var expected = [{
                     column: 20,
-                    line: 1,
-                    message: 'Operators should not be preceded nor followed by any space.'
+                    message: 'Operators should be preceded and followed by one space.'
                 }];
 
-                var options = {
-                    style: 'none'
-                };
+                return spec.parse(source, function (ast) {
+                    var result = spec.linter.lint(options, ast.root.first);
 
-                ast = parseAST(source);
-                ast = ast.first();
-
-                result = linter.lint(options, ast);
-
-                expect(result).to.deep.equal(expected);
+                    expect(result).to.deep.equal(expected);
+                });
             });
 
-            it('should not report on minus shorthand values', function () {
-                var source = '.foo { margin:-10px -1px 0px 20px; }';
-                var result;
-                var ast;
+            it('should not allow missing space after operator', function () {
+                var source = 'height: calc(10px +10px);';
+                var expected = [{
+                    column: 22,
+                    message: 'Operators should be preceded and followed by one space.'
+                }];
 
-                var options = {
-                    style: 'both'
-                };
+                return spec.parse(source, function (ast) {
+                    var result = spec.linter.lint(options, ast.root.first);
 
-                ast = parseAST(source);
-                ast = ast.first();
+                    expect(result).to.deep.equal(expected);
+                });
+            });
 
-                result = linter.lint(options, ast);
+            it('should not allow missing space before and after operator', function () {
+                var source = 'height: calc(10px+10px);';
+                var expected = [{
+                    column: 20,
+                    message: 'Operators should be preceded and followed by one space.'
+                }];
 
-                expect(result).to.be.undefined;
+                return spec.parse(source, function (ast) {
+                    var result = spec.linter.lint(options, ast.root.first);
+
+                    expect(result).to.deep.equal(expected);
+                });
+            });
+
+            it('should not report on negative values', function () {
+                var source = 'margin-left: -10px;';
+
+                return spec.parse(source, function (ast) {
+                    var result = spec.linter.lint(options, ast.root.first);
+
+                    expect(result).to.be.undefined;
+                });
+            });
+
+            it('should not report on negative values in shorthands', function () {
+                var source = 'margin: -10px -1px 0px 20px;';
+
+                return spec.parse(source, function (ast) {
+                    var result = spec.linter.lint(options, ast.root.first);
+
+                    expect(result).to.be.undefined;
+                });
+            });
+
+            it('should not report on negative number at the end of the value', function () {
+                var source = '@foo: (@bar / 3) * -1;';
+
+                return spec.parse(source, function (ast) {
+                    var result = spec.linter.lint(options, ast.root.first);
+
+                    expect(result).to.be.undefined;
+                });
+            });
+
+            it('should not report on negative number at the end of brackets', function () {
+                var source = '@b: spin(@a, -2%);';
+
+                return spec.parse(source, function (ast) {
+                    var result = spec.linter.lint(options, ast.root.first);
+
+                    expect(result).to.be.undefined;
+                });
             });
 
             it('should not report on font-size/line-height shorthand declaration', function () {
-                var source = '.foo { font: 12px/1.5 Arial; }';
-                var result;
-                var ast;
+                var source = 'font: 12px/1.5 Arial;';
 
-                var options = {
-                    style: 'both'
-                };
+                return spec.parse(source, function (ast) {
+                    var result = spec.linter.lint(options, ast.root.first);
 
-                ast = parseAST(source);
-                ast = ast.first();
-
-                result = linter.lint(options, ast);
-
-                expect(result).to.be.undefined;
+                    expect(result).to.be.undefined;
+                });
             });
-        });
+        }); // "both"
 
-        it('should throw on invalid "style" value', function () {
-            var source = '.foo { height: calc(10px+10px); }';
-            var lint;
-            var ast;
+        describe('when "style" is "none"', function () {
+            beforeEach(function () {
+                options = {
+                    style: 'none'
+                };
+            });
 
-            var options = {
-                style: 'invalid'
-            };
+            it('should allow missing space before and after operator', function () {
+                var source = 'height: calc(10px+10px);';
 
-            ast = parseAST(source);
-            ast = ast.first();
+                return spec.parse(source, function (ast) {
+                    var result = spec.linter.lint(options, ast.root.first);
 
-            lint = linter.lint.bind(null, options, ast);
+                    expect(result).to.be.undefined;
+                });
+            });
 
-            expect(lint).to.throw(Error);
-        });
+            it('should not allow one space before operator', function () {
+                var source = 'height: calc(10px +10px);';
+                var expected = [{
+                    column: 21,
+                    message: 'Operators should not be preceded nor followed by any space.'
+                }];
+
+                return spec.parse(source, function (ast) {
+                    var result = spec.linter.lint(options, ast.root.first);
+
+                    expect(result).to.deep.equal(expected);
+                });
+            });
+
+            it('should not allow one space after operator', function () {
+                var source = 'height: calc(10px+ 10px);';
+                var expected = [{
+                    column: 22,
+                    message: 'Operators should not be preceded nor followed by any space.'
+                }];
+
+                return spec.parse(source, function (ast) {
+                    var result = spec.linter.lint(options, ast.root.first);
+
+                    expect(result).to.deep.equal(expected);
+                });
+            });
+
+            it('should not allow a space after operator in shorthand values', function () {
+                var source = 'margin: 10px - 1px 0px 20px;';
+                var expected = [{
+                    column: 16,
+                    message: 'Operators should not be preceded nor followed by any space.'
+                }];
+
+                return spec.parse(source, function (ast) {
+                    var result = spec.linter.lint(options, ast.root.first);
+
+                    expect(result).to.deep.equal(expected);
+                });
+            });
+        }); // "none"
+
+        describe('with invalid "style" value', function () {
+            beforeEach(function () {
+                options = {
+                    style: 'invalid'
+                };
+            });
+
+            it('should throw an error', function () {
+                var source = 'height: calc(10px+10px);';
+
+                return spec.parse(source, function (ast) {
+                    var node = ast.root.first;
+                    var lint = spec.linter.lint.bind(null, options, node);
+
+                    expect(lint).to.throw(Error);
+                });
+            });
+        }); // "invalid"
     });
 });
