a7cc6088698a5f51cddf7b5922be1261b2257b26
Jonathan Wilsson <jonathan.wilsson@gmail.com>
2016-08-20 08:45:23 +0200

    Add missing files

---
+++ test/specs/linters/newline_after_block.js
@@ -0,0 +1,120 @@
+'use strict';
+
+var expect = require('chai').expect;
+var spec = require('../util.js').setup();
+
+describe('lesshint', function () {
+    describe('#newlineAfterBlock()', function () {
+        it('should have the proper node types', function () {
+            var source = '.foo { color: red; }';
+
+            return spec.parse(source, function (ast) {
+                expect(spec.linter.nodeTypes).to.include(ast.root.first.type);
+            });
+        });
+
+        it('should not allow adjacent blocks without a new line', function () {
+            var source = '';
+            var expected = [{
+                message: 'All blocks should be followed by a new line.'
+            }];
+
+            source += '.foo {';
+            source += '    color: red;';
+            source += '}';
+            source += '.bar {';
+            source += '    color: blue;';
+            source += '}';
+
+            return spec.parse(source, function (ast) {
+                var result = spec.linter.lint({}, ast.root.last);
+
+                expect(result).to.deep.equal(expected);
+            });
+        });
+
+        it('should allow adjacent blocks with a new line', function () {
+            var source = '';
+
+            source += '.foo {';
+            source += '    color: red;';
+            source += '}';
+            source += '\n\n';
+            source += '.bar {';
+            source += '    color: blue;';
+            source += '}';
+
+            return spec.parse(source, function (ast) {
+                var result = spec.linter.lint({}, ast.root.last);
+
+                expect(result).to.be.undefined;
+            });
+        });
+
+        it('should not allow adjacent blocks without a new line', function () {
+            var source = '';
+            var expected = [{
+                message: 'All blocks should be followed by a new line.'
+            }];
+
+            source += '.bar {';
+            source += '    color: blue;';
+            source += '}';
+            source += '@media (screen) {';
+            source += '}';
+
+            return spec.parse(source, function (ast) {
+                var result = spec.linter.lint({}, ast.root.last);
+
+                expect(result).to.deep.equal(expected);
+            });
+        });
+
+        it('should allow adjacent at-rule blocks with a new line', function () {
+            var source = '';
+
+            source += '.bar {';
+            source += '    color: blue;';
+            source += '}';
+            source += '\n\n';
+            source += '@media (screen) {';
+            source += '}';
+
+            return spec.parse(source, function (ast) {
+                var result = spec.linter.lint({}, ast.root.last);
+
+                expect(result).to.be.undefined;
+            });
+        });
+
+        it('should not check at-rules without a body', function () {
+            var source = '@import "foo";';
+
+            return spec.parse(source, function (ast) {
+                var result = spec.linter.lint({}, ast.root.last);
+
+                expect(result).to.be.undefined;
+            });
+        });
+
+        it('should allow preceding comments without a new line', function () {
+            var source = '';
+
+            source += '.foo {';
+            source += '    color: red;';
+            source += '}';
+            source += '\n\n';
+            source += '// A comment';
+            source += '\n';
+            source += '.bar {';
+            source += '    color: blue;';
+            source += '}';
+
+            return spec.parse(source, function (ast) {
+                var result = spec.linter.lint({}, ast.root.last);
+
+                expect(result).to.be.undefined;
+            });
+        });
+    });
+});
