6ad80a766c4dfeb7cea9bb0b82b091352827e69e
Jonathan Wilsson <jonathan.wilsson@gmail.com>
2016-08-20 12:21:22 +0200

    Ensure that stringQuotes checks at-rules. Closes #224

--- test/specs/linters/string_quotes.js
+++ test/specs/linters/string_quotes.js
@@ -249,6 +249,70 @@ describe('lesshint', function () {
             });
         });
 
+        it('should not allow double quotes in @-rules when "style" is "single"', function () {
+            var source = '@import "foo.less";';
+            var expected = [{
+                column: 9,
+                line: 1,
+                message: 'Strings should use single quotes.'
+            }];
+
+            var options = {
+                style: 'single'
+            };
+
+            return spec.parse(source, function (ast) {
+                var result = spec.linter.lint(options, ast.root.first);
+
+                expect(result).to.deep.equal(expected);
+            });
+        });
+
+        it('should not allow single quotes in @-rules when "style" is "double"', function () {
+            var source = "@import 'foo.less';";
+            var expected = [{
+                column: 9,
+                line: 1,
+                message: 'Strings should use double quotes.'
+            }];
+
+            var options = {
+                style: 'double'
+            };
+
+            return spec.parse(source, function (ast) {
+                var result = spec.linter.lint(options, ast.root.first);
+
+                expect(result).to.deep.equal(expected);
+            });
+        });
+
+        it('should allow single quotes in @-rules when "style" is "single"', function () {
+            var source = "@import 'foo.less';";
+            var options = {
+                style: 'single'
+            };
+
+            return spec.parse(source, function (ast) {
+                var result = spec.linter.lint(options, ast.root.first);
+
+                expect(result).to.deep.undefined;
+            });
+        });
+
+        it('should allow double quotes in @-rules when "style" is "double"', function () {
+            var source = '@import "foo.less";';
+            var options = {
+                style: 'double'
+            };
+
+            return spec.parse(source, function (ast) {
+                var result = spec.linter.lint(options, ast.root.first);
+
+                expect(result).to.deep.undefined;
+            });
+        });
+
         it('should throw on invalid "style" value', function () {
             var source = ".foo { content: 'Hello world' }";
             var options = {
