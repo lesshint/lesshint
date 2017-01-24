73482c1ddd17accf1a48610ef04eac6f2c79ad22
Jonathan Wilsson <jonathan.wilsson@gmail.com>
2017-01-24 15:36:12 +0100

    Expose Lesshint and Runner classes in the public API

---
+++ test/specs/index.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const expect = require('chai').expect;
+
+describe('lesshint', function () {
+    const api = require('../../lib/index');
+
+    it('should expose Lesshint class in public API', function () {
+        expect(api.Lesshint).to.be.a('function');
+    });
+
+    it('should expose Runner class in public API', function () {
+        expect(api.Runner).to.be.a('function');
+    });
+});
