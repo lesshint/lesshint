b69ff9848ead0621bd71795bcacaf12309588252
Marek Duława <wareczek@gmail.com>
2017-04-26 14:23:19 +0200

    Fix spaceAfterPropertyValue bug with !important (#379)
    Closes #368

--- test/specs/linters/space_after_property_value.js
+++ test/specs/linters/space_after_property_value.js
@@ -26,8 +26,40 @@ describe('lesshint', function () {
             });
         });
 
+        it('should allow a missing space after "!important" when "style" is "no_space"', function () {
+            const source = '.foo { color: red !important; }';
+            const options = {
+                style: 'no_space'
+            };
+
+            return spec.parse(source, function (ast) {
+                const result = spec.linter.lint(options, ast.root.first.first);
+
+                expect(result).to.be.undefined;
+            });
+        });
+
         it('should not allow any space when "style" is "no_space"', function () {
             const source = '.foo { color: red ; }';
+            const expected = [{
+                column: 18,
+                line: 1,
+                message: 'Semicolon after property value should not be preceded by any space.'
+            }];
+
+            const options = {
+                style: 'no_space'
+            };
+
+            return spec.parse(source, function (ast) {
+                const result = spec.linter.lint(options, ast.root.first.first);
+
+                expect(result).to.deep.equal(expected);
+            });
+        });
+
+        it('should not allow any space after "!important" when "style" is "no_space"', function () {
+            const source = '.foo { color: red !important ; }';
             const expected = [{
                 column: 18,
                 line: 1,
@@ -58,6 +90,19 @@ describe('lesshint', function () {
             });
         });
 
+        it('should allow one space after "!important" when "style" is "one_space"', function () {
+            const source = '.foo { color: red !important ; }';
+            const options = {
+                style: 'one_space'
+            };
+
+            return spec.parse(source, function (ast) {
+                const result = spec.linter.lint(options, ast.root.first.first);
+
+                expect(result).to.be.undefined;
+            });
+        });
+
         it('should not allow a missing space when "style" is "one_space"', function () {
             const source = '.foo { color: red; }';
             const expected = [{
@@ -77,6 +122,24 @@ describe('lesshint', function () {
             });
         });
 
+        it('should not allow a missing space after "!important" when "style" is "one_space"', function () {
+            const source = '.foo { color: red !important; }';
+            const expected = [{
+                column: 18,
+                line: 1,
+                message: 'Semicolon after property value should be preceded by one space.'
+            }];
+
+            const options = {
+                style: 'one_space'
+            };
+
+            return spec.parse(source, function (ast) {
+                const result = spec.linter.lint(options, ast.root.first.first);
+
+                expect(result).to.deep.equal(expected);
+            });
+        });
 
         it('should throw on invalid "style" value', function () {
             const source = '.foo { color: red ; }';
