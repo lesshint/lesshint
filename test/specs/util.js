d4d25aa5d194326f58c12055d384b84fdc9f3226
Jonathan Wilsson <jonathan.wilsson@gmail.com>
2017-05-18 16:46:52 +0200

    Cleanup after postcss-less 1.0

--- test/specs/util.js
+++ test/specs/util.js
@@ -23,37 +23,11 @@ module.exports = {
             return {};
         }
 
-        // Tests selectors for pseudo classes/selectors. eg. :not(), :active
-        const rPseudo = /::?[^ ,:.]+/g;
-
         return {
             linter: rule,
             parser: linter.parse,
             parse: function (source, callback) {
                 return linter.parse(source).then(function (ast) {
-                    /**
-                     * If we're dealing with a regular Rule (or other) node, which isn't
-                     * an actual Mixin or AtRule, and its selector contains a pseudo
-                     * class or selector, then clean up the raws and params properties.
-                     * If we don't have this here, then the tests never get the same
-                     * modified nodes.
-                     * Tracking: https://github.com/webschik/postcss-less/issues/56
-                     * TODO: remove this when issue resolved
-                     */
-                    ast.root.walk(function (node) {
-                        if (node.params && rPseudo.test(node.selector)) {
-                            delete node.params;
-
-                            /**
-                             * This just started showing up in postcss-less@0.14.0. not sure
-                             * if it's sticking around, but making sure we're thorough.
-                             */
-                            if (node.raws.params) {
-                                delete node.raws.params;
-                            }
-                        }
-                    });
-
                     callback && callback(ast);
                 });
             }
