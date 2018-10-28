bc7cd2314c9ab514e1f1f065517d6053374a546d
Tiross <Tiross@users.noreply.github.com>
2018-10-28 23:59:11 +0100

    Fix eslint on gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const gulp = require('gulp');
 
 gulp.task('lint', () => {
@@ -11,10 +13,11 @@ gulp.task('lint', () => {
 
 gulp.task('test', ['lint'], () => {
     const mocha = require('gulp-mocha');
+    const options = {
+        read: false
+    };
 
-    return gulp.src(['./test/specs/**/*.js', '!./test/specs/util.js'], {
-            read: false
-        })
+    return gulp.src(['./test/specs/**/*.js', '!./test/specs/util.js'], options)
         .pipe(mocha());
 });
 
